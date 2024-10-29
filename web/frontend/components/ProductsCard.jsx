0b858b3e77ae05b0661ed3b7831cf339a7ce1417
Prince Baghel <pkbghl90@gmail.com>
2024-10-29 12:47:07 +0530

    chore: testing accessToken

--- web/frontend/components/ProductsCard.jsx
+++ web/frontend/components/ProductsCard.jsx
@@ -22,6 +22,7 @@ export function ProductsCard() {
     },
     refetchOnWindowFocus: false,
   });
+  console.log("productdata", data)
 
   const setPopulating = (flag) => {
     shopify.loading(flag);
